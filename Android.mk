2713ca306f4907ae920878eddff6744cf7a2b360
Colin Courtney <ccour004@gmail.com>
2017-07-08 22:40:21 -0400

    Fixed Android.mk.

--- Android.mk
+++ Android.mk
@@ -3,17 +3,17 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := gdxpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/core/src
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/src
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 
 LOCAL_SRC_FILES := \
 	$(subst $(LOCAL_PATH)/,, \
-	$(wildcard $(LOCAL_PATH)/core/src/*.cpp) \
-	$(wildcard $(LOCAL_PATH)/core/src/math/*.cpp) \
-	$(wildcard $(LOCAL_PATH)/core/src/math/collision/*.cpp) \
-	$(wildcard $(LOCAL_PATH)/core/src/graphics/*.cpp) \
-	$(wildcard $(LOCAL_PATH)/core/src/graphics/glutils/*.cpp))
+	$(wildcard $(LOCAL_PATH)/src/*.cpp) \
+	$(wildcard $(LOCAL_PATH)/src/math/*.cpp) \
+	$(wildcard $(LOCAL_PATH)/src/math/collision/*.cpp) \
+	$(wildcard $(LOCAL_PATH)/src/graphics/*.cpp) \
+	$(wildcard $(LOCAL_PATH)/src/graphics/glutils/*.cpp))
 
 LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES
 LOCAL_CPP_FEATURES := rtti exceptions
